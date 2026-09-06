70c9c2f236d8a0158256b8e85b893a2d517b6658
Nityansh Rupesh Bahadur <nityansh.bahadur1905@gmail.com>
2026-09-06 18:04:19 +0530

    chore: remove duplicate onboarding module

--- src/domain/onboarding-v2.ts
+++
@@ -1,2 +0,0 @@
-export { ONBOARDING_KEY, AUTH_KEY, defaultOnboardingProfile, loadOnboarding, saveOnboarding, loadAuth, saveAuth, clearAuth } from './onboarding';
-export type { OnboardingProfile } from './onboarding';
